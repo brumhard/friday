e235c85c2b0ea3fc0b4c3caa958fb6cd9e9deb23
Tobias Brumhard <code@brumhard.com>
2023-04-07 22:26:13 +0200

    Add packages and apps, disable integration tests

--- flake.nix
+++ flake.nix
@@ -1,37 +1,64 @@
 {
-  description = "local development setup";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     utils.url = "github:numtide/flake-utils";
   };
 
-  # see here for builds: https://serokell.io/blog/practical-nix-flakes
-  # or https://nix-community.github.io/dream2nix/subsystems/rust.html
-
   outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      # see https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/9
-        # -> might be nice to switch to https://github.com/oxalica/rust-overlay at some time
-      with pkgs;
-      {
-        devShell = mkShell {
-          packages = [
-            rustc
-            cargo
-            rustfmt
-            rust-analyzer
-            clippy
-          ];
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          name = "friday";
+          version = "latest";
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
+        rec {
+          packages = {
+            default = packages.${name};
+            "${name}" = rustPlatform.buildRustPackage {
+              pname = name;
+              version = version;
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
 
-          shellHook = ''
-            export RUST_SRC_PATH="${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-            export FRIDAY_FILE=testing
-          '';
-        };
-      }
-    );
+              meta = with lib; {
+                description = "Manage stuff to do on fridays.";
+                homepage = "https://github.com/brumhard/${name}";
+                maintainers = with maintainers; [ brumhard ];
+                license = licenses.mit;
+              };
+            };
+          };
+
+          apps = {
+            default = apps.cli;
+            cli = utils.lib.mkApp {
+              drv = packages.default;
+              exePath = "/bin/cli";
+            };
+            server = utils.lib.mkApp {
+              drv = packages.default;
+              exePath = "/bin/server";
+            };
+          };
+
+          devShell = mkShell {
+            packages = [
+              rustc
+              cargo
+              rustfmt
+              rust-analyzer
+              clippy
+            ];
+
+            shellHook = ''
+              export RUST_SRC_PATH="${rustPlatform.rustLibSrc}";
+              export FRIDAY_FILE=testing
+            '';
+          };
+        }
+      );
 }
