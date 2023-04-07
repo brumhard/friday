22a955526db7abdaeedf35491e1d314c3ba9d146
Tobias Brumhard <code@brumhard.com>
2023-04-07 23:13:17 +0200

    Fix integration tests

--- tests/cli_integration_test.rs
+++ tests/cli_integration_test.rs
@@ -2,50 +2,54 @@ use std::{error::Error, fs};
 
 use assert_cmd::Command;
 
-// fn friday_cli() -> Command {
-//     Command::cargo_bin("cli").unwrap()
-// }
+fn friday_cli() -> Command {
+    Command::cargo_bin("cli").unwrap()
+}
 
-// // NOTE: it would also be possible to move the code in main.rs to lib.rs and only call
-// // run in the main.rs. Then, run could be tested here without the need to really run
-// // a binary here.
-// // This is documented here: https://doc.rust-lang.org/book/ch11-03-test-organization.html#integration-tests-for-binary-crates.
-// //
-// // Anyways, it's nice to test the whole interface of the CLI as well e2e.
-// #[test]
-// fn it_prints_help_on_empty_action() {
-//     let cmd = friday_cli().assert();
-//     let output = cmd.get_output();
-//     let output_str = String::from_utf8_lossy(&output.stdout).to_string();
-//     assert!(
-//         output_str.contains("manage stuff to do on fridays"),
-//         "expected '{}' to contain 'manage stuff to do on fridays'",
-//         output_str
-//     )
-// }
+// NOTE: it would also be possible to move the code in main.rs to lib.rs and only call
+// run in the main.rs. Then, run could be tested here without the need to really run
+// a binary here.
+// This is documented here: https://doc.rust-lang.org/book/ch11-03-test-organization.html#integration-tests-for-binary-crates.
+//
+// Anyways, it's nice to test the whole interface of the CLI as well e2e.
+#[test]
+fn it_prints_help_on_empty_action() {
+    let tmp_dir = tempdir::TempDir::new("testing").unwrap();
+    let cmd = friday_cli()
+        .env("FRIDAY_FILE", tmp_dir.path().join("sth"))
+        .assert()
+        .success();
+    let output = cmd.get_output();
+    let output_str = String::from_utf8_lossy(&output.stdout).to_string();
+    assert!(
+        output_str.contains("manage stuff to do on fridays"),
+        "expected '{}' to contain 'manage stuff to do on fridays'",
+        output_str
+    )
+}
 
-// #[test]
-// fn it_adds_to_file() -> Result<(), Box<dyn Error>> {
-//     let tmp_dir = tempdir::TempDir::new("testing")?;
-//     let file_path = tmp_dir.path().join("friday.md");
-//     let to_add = "something that should be added";
-//     friday_cli()
-//         .arg("add")
-//         .arg(to_add)
-//         .env("FRIDAY_FILE", file_path.to_string_lossy().to_string())
-//         .assert()
-//         .success();
+#[test]
+fn it_adds_to_file() -> Result<(), Box<dyn Error>> {
+    let tmp_dir = tempdir::TempDir::new("testing")?;
+    let file_path = tmp_dir.path().join("friday.md");
+    let to_add = "something that should be added";
+    friday_cli()
+        .arg("add")
+        .arg(to_add)
+        .env("FRIDAY_FILE", &file_path)
+        .assert()
+        .success();
 
-//     let content = fs::read_to_string(file_path)?;
-//     assert!(
-//         content.contains(to_add),
-//         "expected '{}' to contain the added string",
-//         content
-//     );
-//     assert!(
-//         content.contains("# It's friday my dudes"),
-//         "expected file content to contain header",
-//     );
-//     // assert that file contains string
-//     Ok(())
-// }
+    let content = fs::read_to_string(&file_path)?;
+    assert!(
+        content.contains(to_add),
+        "expected '{}' to contain the added string",
+        content
+    );
+    assert!(
+        content.contains("# It's friday my dudes"),
+        "expected file content to contain header",
+    );
+    // assert that file contains string
+    Ok(())
+}
